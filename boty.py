6ce566c8e2d07480fe24ff579bce10a76f2ca0c0
pqdzz <mkode172@gmail.com>
2026-05-08 16:53:31 +0300

    Update boty.py

--- boty.py
+++ boty.py
@@ -8,7 +8,7 @@ import time
 from datetime import datetime, timedelta
 
 # --- الإعدادات ---
-API_TOKEN = '8260522692:AAFt81cAPzjbNOHyqzgWJRzFTNc_FU84X0U'
+API_TOKEN = ''8260522692:AAFt81cAPzjbNOHyqzgWJRzFTNc_FU84X0U'
 ADMIN_ID = 96799666 
 bot = telebot.TeleBot(API_TOKEN)
 
